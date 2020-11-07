a0f1b672d096cd23328e3176b0a9b1e585d955c5
varandrew <varandrewchen@outlook.com>
2020-11-07 12:20:10 +0800

    feat(request): add request.

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,2 +1,47 @@
-import axios from 'axios'
+import { AxiosRequestConfig } from 'axios'
 
+export const BaseUrl = () => {
+  switch (process.env.NODE_ENV) {
+    case 'development':
+      return 'http://localhost:3000'
+    default:
+      return ''
+  }
+}
+
+export const NoConsole = false
+
+const axiosConfig: AxiosRequestConfig = {
+  baseURL: BaseUrl(),
+  // 请求后的数据处理
+  // 超时设置s
+  timeout: 30000,
+  headers: {
+    get: {
+      'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+      // 在开发中，一般还需要单点登录或者其他功能的通用请求头，可以一并配置进来
+    },
+    post: {
+      'Content-Type': 'application/json;charset=utf-8',
+      // 在开发中，一般还需要单点登录或者其他功能的通用请求头，可以一并配置进来
+    },
+  },
+  // 跨域是否带Token
+  withCredentials: true,
+  responseType: 'json',
+  // xsrf 设置
+  xsrfCookieName: 'XSRF-TOKEN',
+  xsrfHeaderName: 'X-XSRF-TOKEN',
+  // 最多转发数，用于node.js
+  maxRedirects: 5,
+  // 最大响应数据大小
+  maxContentLength: 2000,
+  // 自定义错误状态码范围
+  validateStatus() {
+    // 使用async-await，处理reject情况较为繁琐，所以全部返回resolve，在业务代码中处理异常
+    return true
+    // return status >= 200 && status < 300
+  },
+}
+
+export default axiosConfig
